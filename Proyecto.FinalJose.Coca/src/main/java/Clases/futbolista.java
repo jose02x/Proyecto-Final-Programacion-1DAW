44d74070503d9b4a791a65ddb5ed7fe8dfa81314
jose02x <92530070+jose02x@users.noreply.github.com>
2022-06-04 10:31:36 +0200

    Añadiendo las cosas ya realizadas
    proyecto y el dia

---
+++ Proyecto.FinalJose.Coca/src/main/java/Clases/futbolista.java
@@ -0,0 +1,7 @@
+package Clases;
+
+public class futbolista {
+	private int precio;
+	
+
+}
---
+++ Proyecto.FinalJose.Coca/src/main/java/Clases/jugador.java
@@ -0,0 +1,8 @@
+package Clases;
+
+public class jugador {
+	private byte edad;
+	private String constraseña;
+	private String correo;
+
+}
